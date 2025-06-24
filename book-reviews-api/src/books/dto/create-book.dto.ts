7aa790de4b05624c6241d593ecc8c173e1c3e9e4
WolfgänG <felipefernandes.sjc@hotmail.com>
2025-06-23 21:19:03 -0300

    Adds validation to book creation

--- book-reviews-api/src/books/dto/create-book.dto.ts
+++ book-reviews-api/src/books/dto/create-book.dto.ts
@@ -1,5 +1,15 @@
+import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
+
 export class CreateBookDto {
+  @IsString()
+  @IsNotEmpty({ message: 'Title cannot be empty.' })
   title: string;
+
+  @IsString()
+  @IsNotEmpty({ message: 'Author cannot be empty.' })
   author: string;
+
+  @IsString()
+  @IsOptional()
   isbn?: string;
 }
