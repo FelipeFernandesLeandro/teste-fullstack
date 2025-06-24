91b935a8b406c15101b5db22b29872370b3dc897
WolfgänG <felipefernandes.sjc@hotmail.com>
2025-06-24 13:29:29 -0300

    Setup Jest configuration and testing setup for the frontend

---
+++ book-reviews-ui/jest.config.mjs
@@ -0,0 +1,16 @@
+import nextJest from 'next/jest.js'
+
+const createJestConfig = nextJest({
+  dir: './',
+})
+
+const config = {
+  testEnvironment: 'jest-environment-jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  moduleNameMapper: {
+    '^@/components/(.*)$': '<rootDir>/src/components/$1',
+    '^@/lib/(.*)$': '<rootDir>/src/lib/$1',
+  },
+}
+
+export default createJestConfig(config)
